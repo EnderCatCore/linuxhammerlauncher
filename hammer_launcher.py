cc3d549bb38791ddaa5ae1074adb1ba69a43c87e
Artshine Aura <isaacboygenius@gmail.com>
2026-03-26 02:22:46 -0500

    slightly updated todo comment in python file and deleted reminder png 	modified:   hammer_launcher.py 	deleted:    thingformyselftorememberthecommand.png

--- hammer_launcher.py
+++ hammer_launcher.py
@@ -37,6 +37,10 @@ or brute force sleep(9999999)
 
 -HL2 wont compile at all. Can't find steam app user info error
 
+-i dont imagine itll make a difference but testing on x11 should be done probably
+
+-also not sure if this makes a difference but testing on an nvidia machine is also possibly a good idea
+
 '''
 
 
