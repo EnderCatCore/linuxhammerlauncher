436f9dd2c4e92c672c032eedac709d0697b4501a
Artshine Aura <isaacboygenius@gmail.com>
2026-03-27 09:23:01 -0500

    okay its centered again. still working on vgui

--- hammer_launcher.py
+++ hammer_launcher.py
@@ -678,7 +678,7 @@ def rendermainwindow():
     Frame creation
     '''
     optionsframe = Frame(root, bg="#3e4637", relief='sunken', bd=0, highlightthickness=0)
-    optionsframe.grid(row=1, column=0,sticky="we")
+    optionsframe.grid(row=1, column=1,sticky="we")
     optionsframe.grid_columnconfigure(0, minsize=23, weight=0)
     optionsframe.grid_columnconfigure(1, minsize=211, weight=1)
     '''
